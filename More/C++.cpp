c57ef8ebe615a3b951865803af54537bb7ae0ba0
lilbruh-save <painkiller06@bk.ru>
2024-10-27 23:36:30 +0300

    fix

--- More/C++.cpp
+++ More/C++.cpp
@@ -1,11 +1,20 @@
-#include <iostream>
 #include <string>
+#include <iostream>
 
-using namespace std;
+int main() {
+    // Ввод строки
+    std::string line;
+    std::getline(std::cin, line);
 
-int main(){
-  setlocale(LC_ALL, "Russian");
+    // Ввод числа и символа
+    int number;
+    char symbol;
+    std::cin >> number >> symbol;
+    line[number] = symbol;
 
-  
+
+    // Вывод полученной строки
+    std::cout << line << std::endl;
+
+    return 0;
 }
-
