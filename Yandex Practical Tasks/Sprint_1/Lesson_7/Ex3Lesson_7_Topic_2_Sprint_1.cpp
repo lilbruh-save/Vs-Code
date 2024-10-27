10ad073607e5715bfa943e5e9b236da1831cd5fb
lilbruh-save <painkiller06@bk.ru>
2024-10-27 12:29:17 +0300

    class_27.10

---
+++ Yandex Practical Tasks/Sprint_1/Lesson_7/Ex3Lesson_7_Topic_2_Sprint_1.cpp
@@ -0,0 +1,25 @@
+/*Программа, которую вы напишете, пригодится при ремонте. Она позволит рассчитать количество рулонов обоев, необходимых, чтобы обклеить комнату.*/
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+    double length_ceil, width_ceil, height_ceil;
+    double width_roll, length_roll;
+    double S_ceil, S_roll, answer;
+
+    // Ввод данных
+    std::cin >> length_ceil >> width_ceil >> height_ceil >> width_roll >> length_roll;
+
+    // Вычисление площади потолка и рулона
+    S_ceil = ((length_ceil + width_ceil) * 2) * height_ceil;
+    S_roll = width_roll * length_roll;
+
+    // Подсчет количества рулонов
+    answer = S_ceil / S_roll;
+
+    // Вывод результата
+    std::cout << std::ceil(answer) << std::endl;
+
+    return 0;
+}
