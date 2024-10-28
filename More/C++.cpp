b235af979ef1016a758d015b2be80b4b53845ae1
lilbruh-save <painkiller06@bk.ru>
2024-10-28 16:41:47 +0300

    fix

--- More/C++.cpp
+++ More/C++.cpp
@@ -1,7 +1,1 @@
-#include <iostream>
 
-int main() {
-    int number;
-    std::cin >> number;
-    if (number % 2  == 0){ std::cout << "Число "<< number << " чётное";}
-    else{ std::cout << "Число "<< number << " нечётное";}
