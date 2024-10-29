afca5d6be06089b3944f053e3afb13cbd4216b53
lilbruh-save <painkiller06@bk.ru>
2024-10-29 09:45:00 +0300

    fix

---
+++ Yandex Practical Tasks/Sprint_1/Topic_3/Lesson_4/Ex1Lesson_4_Topic_3_Sprint_1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    enum class Weekdays {
+        Monday = 1,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    };
+    // Здесь нужно написать код.
+}
