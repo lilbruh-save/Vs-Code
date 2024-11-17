db6556d1880038fac4e582fe5a14df578a55666e
lilbruh-save <painkiller06@bk.ru>
2024-11-17 22:38:58 +0300

    fix

--- Yandex Practical Tasks/Sprint_1/Topic_5/Lesson_2/Ex1Lesson_2_Topic_5_Sprint_1.cpp
+++ Yandex Practical Tasks/Sprint_1/Topic_5/Lesson_2/Ex1Lesson_2_Topic_5_Sprint_1.cpp
@@ -1,12 +1,24 @@
-// Просто написать программу которая менят регистры
-//PrintUpper — принимает в качестве параметра строку и выводит её в верхнем регистре.
-//PrintLower — принимает в качестве параметра строку и выводит её в нижнем регистре.
-//Для преобразования символов воспользуйтесь функциями std::toupper и std::tolower из заголовочного файла cctype.
+    // Просто написать программу которая менят регистры
+    //PrintUpper — принимает в качестве параметра строку и выводит её в верхнем регистре.
+    //PrintLower — принимает в качестве параметра строку и выводит её в нижнем регистре.
+    //Для преобразования символов воспользуйтесь функциями std::toupper и std::tolower из заголовочного файла cctype.
 #include <iostream>
 #include <string>
 #include <cctype>
 
-//Написаить функции
+void PrintUpper(const std::string& str) {
+    for (unsigned char c : str) {
+        std::cout << static_cast<char>(std::toupper(c));
+    }
+    std::cout << std::endl;
+}
+
+void PrintLower(const std::string& str) {
+    for (unsigned char c : str) {
+        std::cout << static_cast<char>(std::tolower(c));
+    }
+    std::cout << std::endl;
+}
 
 int main() {
     std::string str;
