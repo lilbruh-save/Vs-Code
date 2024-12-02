b4a5a37475813ea0e626d8205b9b98dda0d5eb4e
lilbruh-save <painkiller06@bk.ru>
2024-12-02 05:51:08 +0300

    fix02.11.2024

---
+++ Yandex Practical Tasks/Sprint_2/Topic_2/Lesson_2/Ex1Lesson_2_Topic_1_Sprint_2.cpp
@@ -0,0 +1,23 @@
+// Используйте структуру Person, чтобы объявить в программе двух волшебников: Harry Potter и Hermione Granger. Затем выведите их в стандартный поток вывода:
+#include <iostream>
+
+struct Person
+{
+  std::string name;
+  std::string surname;
+};
+
+using namespace std::literals;
+
+int main()
+{
+  Person person1;
+  // Не меняйте код выше этой строки!
+
+  // 1) Пусть person1 зовут Harry Potter.
+
+  // 2) Объявите переменную person2 типа Person, пусть второго человека зовут Hermione Granger.
+
+  // 3) Выведите через пробел имя и фамилию первого человека.
+  //    Во второй строке выведите через пробел имя и фамилию второго человека.
+}
